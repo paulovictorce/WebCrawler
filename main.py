901d6ef4a5c274b65a40d7b16518a1309e6df536
paulohome <bd.paulovictor@gmail.com>
2018-01-22 17:46:17 -0300

    Inserindo Parametros de entrada

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import requests
 from lxml import html
+import os
 
 print 'URL : '
 url = raw_input()
+print 'Lang : '
+lang = raw_input()
+
+if(lang == 'pt'):
+    url = url + '?ned=pt-BR_br&hl=pt-BR&gl=BR'
+else:
+    url = url + '?ned=us&gl=US&hl=en'
+
+print url
+
 response = requests.get(url)
+f = open('titles.txt', 'w')
 
 if (response.status_code == 200):
     print('Request Ok!')
     page = html.fromstring(response.text)
-    text = page.cssselect('a.nuEeue.hzdq5d.ME7ew')[0].text
-    print text
-
+    # text = page.cssselect('a.nuEeue.hzdq5d.ME7ew')[0].text
+    for a in page.cssselect('a.nuEeue.hzdq5d.ME7ew'):
+        f.writelines(a.text_content().encode('utf-8') + os.linesep)
+    f.close
 else:
-    print('Request Wrong!')
+    print('Request Wrong!') 
